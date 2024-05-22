79a453f03bf4ecdbfe3cd882f8f4e4c8824e18ca
fahmifahrulrozi <110888444+fahmifahrulrozi@users.noreply.github.com>
2024-05-22 15:08:54 +0700

    Add files via upload

---
+++ modul2/latihan1.php
@@ -0,0 +1,8 @@
+<?php
+$nama_lengkap = "Fahmi Fahrulrozi";
+$_gaji = 2000;
+echo "Nama Lengkap : $nama_lengkap <br>";
+echo "Gaji : Rp. $_gaji <br>";
+$_gaji = "dua rebu";
+echo "Terbilang : $_gaji";
+?>
---
+++ modul2/latihan2.php
@@ -0,0 +1,13 @@
+<?php
+$panjang = 10;
+$lebar = 10;
+define("luas_persegi",$panjang*$lebar);
+echo "Luas persegi adalah = ".luas_persegi;
+?>
+
+<?php
+define("panjang",10);
+define("lebar",10);
+$luas = panjang * lebar;
+echo "Luas persegi adalah = $luas";
+?>
